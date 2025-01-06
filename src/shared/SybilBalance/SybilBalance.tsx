cd7db370fc4d1c4a045503e695638e8cf89ff28e
nature-lover-iv <ihor.dreamer@gmail.com>
2025-01-06 13:12:54 +0000

    chore: add tooltip for balance with unrounded value

--- src/shared/SybilBalance/SybilBalance.tsx
+++ src/shared/SybilBalance/SybilBalance.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from '@nextui-org/react';
 import { Typography } from 'antd';
 import { formatBalance } from 'Utils/balance';
 import { useFetchBalance } from 'Services/sybilService';
@@ -6,10 +7,14 @@ export const SybilBalance = () => {
   const { data: balance, isLoading: isBalanceLoading } = useFetchBalance();
 
   return (
-    <Typography.Title level={3}>
-      Balance:{' '}
-      {(!balance || isBalanceLoading) && balance !== 0 ? '...' : formatBalance(balance).toFixed(2)}{' '}
-      USD
-    </Typography.Title>
+    <Tooltip content={balance ? formatBalance(balance) : 0}>
+      <Typography.Title level={3}>
+        Balance:{' '}
+        {(!balance || isBalanceLoading) && balance !== 0
+          ? '...'
+          : formatBalance(balance).toFixed(2)}{' '}
+        USD
+      </Typography.Title>
+    </Tooltip>
   );
 };
